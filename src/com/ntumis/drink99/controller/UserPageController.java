f9014a3f8e14d0dd2414e2576f1f136caea5a07d
mxt305 <mxtsai305shiranui@gmail.com>
2015-12-08 15:53:01 +0800

    example_151208_2

--- src/com/ntumis/drink99/controller/UserPageController.java
+++ src/com/ntumis/drink99/controller/UserPageController.java
@@ -1,10 +1,13 @@
 package com.ntumis.drink99.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class UserPageController
@@ -16,15 +19,26 @@ public abstract class UserPageController extends HttpServlet {
         super();
     }
 
-
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		HttpSession session = request.getSession();
+		boolean isLogin = false;
+		Object o = session.getAttribute("isLogin");
+		if(o != null && o instanceof Boolean){
+			isLogin = (Boolean) o;
+		}
+		if(isLogin){
+			process(request,response);
+		} else {
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/login.jsp");
+			rd.forward(request, response);
+		}
 	}
-
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
+	
+	abstract protected void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException;
 
 	
 
