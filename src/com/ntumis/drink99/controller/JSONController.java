a4079235d1e7e758aa18a6deb5590e2a60b4ce52
mxt305 <mxtsai305shiranui@gmail.com>
2015-12-08 11:46:39 +0800

    example_151208

--- src/com/ntumis/drink99/controller/JSONController.java
+++ src/com/ntumis/drink99/controller/JSONController.java
@@ -1,26 +1,28 @@
 package com.ntumis.drink99.controller;
 
 import java.io.IOException;
-import java.util.Date;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.gson.Gson;
+
 /**
- * Servlet implementation class MainController
+ * Servlet implementation class JSONController
  */
-@WebServlet("/")
-public class MainController extends HttpServlet {
+@WebServlet("/json")
+public class JSONController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public MainController() {
+    public JSONController() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -29,10 +31,17 @@ public class MainController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/main.jsp");
-		Date today = new Date();
-		request.setAttribute("today", today);
-		rd.forward(request, response);
+		response.setHeader("Content-type", "application/json");
+		ArrayList<Data> al = new ArrayList<>();
+		al.add(this.new Data(1,"Mary"));
+		al.add(this.new Data(3,"Joe"));
+		al.add(this.new Data(4,"Ted"));
+		al.add(this.new Data(10,"Tom"));
+		al.add(this.new Data(22,"Candy"));
+		Gson gson = new Gson();
+		String json_string = gson.toJson(al);
+		PrintWriter out = response.getWriter();
+		out.print(json_string);
 	}
 
 	/**
@@ -42,5 +51,12 @@ public class MainController extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-
+	private class Data{
+		public int id;
+		public String name;
+		public Data(int id, String name){
+			this.id = id;
+			this.name = name;
+		}
+	}
 }
--- src/com/ntumis/drink99/controller/UserPageController.java
+++ src/com/ntumis/drink99/controller/UserPageController.java
@@ -16,20 +16,16 @@ public abstract class UserPageController extends HttpServlet {
         super();
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
+
+	
 
 }
