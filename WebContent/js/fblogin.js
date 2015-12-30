d6e2b46bd1f2d7f91550c94fc529ec3565275e89
MonetCheng <s936207s@yahoo.com.tw>
2015-12-31 02:11:25 +0800

    debug
    FB loginButton

--- WebContent/js/fblogin.js
+++ WebContent/js/fblogin.js
@@ -42,5 +42,10 @@ $(function() {
 			scope : "email"
 		});
 	}
-
+	<div
+    class="fb-login-button"
+    data-max-rows="1"
+    data-size="xlarge"
+    data-show-faces="false"
+    data-auto-logout-link="true"></div>
 });
